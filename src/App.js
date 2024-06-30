73e22a8f48f532fa020ae55bf7805121978cdd4c
debghs <dghosh31428@gmail.com>
2024-07-01 02:19:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import ImageRecognition from "./components/ImageRecognition";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container mx-auto p-4">
+        <div className="bg-gray-200 p-6 rounded-md">
+          <h1 className="text-2xl font-bold mb-4">
+            Upload an Image for Recognition
+          </h1>
+          <ImageRecognition />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
